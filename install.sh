885c95a41a88ff959f9827c41389983762caadf7
jguglielmi <Jaison.Guglielmi@soteradefense.com>
2015-03-31 13:26:47 -0400

    updated version of selenium and fitnesse to most recent, updated oasisplugin to handle most current formats of FitNesse

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 export WGET_HOME=./lib/wget-1.11.4-1-bin
 export PATH=$PATH:WGET_HOME/bin
-mvn install:install-file -Dfile=./fitnesse-standalone.jar -DgroupId=org.fitnesse -DartifactId=fitnesse-standalone -Dversion=20140418 -Dpackaging=jar
+mvn install:install-file -Dfile=./fitnesse-standalone.jar -DgroupId=org.fitnesse -DartifactId=fitnesse-standalone -Dversion=20150226 -Dpackaging=jar
 mvn install:install-file -Dfile=./lib/JmeterBundle.jar -DgroupId=JMeterBundle -DartifactId=JMeterBundle -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=./lib/sqljdbc4.jar -DgroupId=sqljdbc4 -DartifactId=sqljdbc4 -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=./plugins/synthuse.jar -DgroupId=synthuse -DartifactId=synthuse -Dversion=1.1.0 -Dpackaging=jar
