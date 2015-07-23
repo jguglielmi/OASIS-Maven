89560261c9e02ade6cd07314d3a871d86f587494
Alex Kogon <alexkogon@gmail.com>
2015-07-23 16:08:40 +0200

    fixed command for debug to server mode

--- debugoasis.sh
+++ debugoasis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Start OASIS on http://localhost:8000/
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:8800,suspend=y -jar oasis-1.0-jar-with-dependencies.jar -d . -p 8000 -o -e 0
+java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=127.0.0.1:8800,suspend=y -jar oasis-1.0-jar-with-dependencies.jar -d . -p 8000 -o -e 0
